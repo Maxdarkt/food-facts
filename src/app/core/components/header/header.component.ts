917f2a7b1550fe87fd6b2723ccc44741be733fc4
Tourneux Maxence <tourneuxmaxence@gmail.com>
2022-09-24 12:18:00 +0200

    HEADER: header implementation (resposive menu), verify addEventListener on resize, config tailwindcss theme extend : screens, fontSize, borderRadius, listStyleType

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  desktop!: boolean;
+  minScreen!: boolean;
+
+  constructor( ) { }
 
   ngOnInit(): void {
+    this.desktop = true;
+    this.minScreen = false;
+
+    this.isDesktop();
+    this.isMinScreen();
+    window.addEventListener('resize', this.listenResize);
+  }
+
+  listenResize(): void {
+    this.desktop = window.innerWidth > 985;
+    this.minScreen = window.innerWidth < 350;
+  }
+  isDesktop(): void {
+    this.desktop = window.innerWidth > 985;
+  }
+  isMinScreen(): void {
+    this.minScreen = window.innerWidth < 350;
+  }
+  menuItem(): void {
+    if(!this.desktop) {
+      const checkboxMenu = document.getElementById("menu-btn")as HTMLInputElement | null;
+      if(checkboxMenu !== null) {
+        checkboxMenu.checked = false;
+      }
+      
+    }
   }
 
 }
